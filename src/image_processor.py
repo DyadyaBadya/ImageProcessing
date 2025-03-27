973886f900f09fea73cac1e4b9fc05d25f2917fe
unknown <admin@507x8n05.mcag.rust>
2025-03-27 11:30:25 +0300

    Upgrade python code

--- src/image_processor.py
+++ src/image_processor.py
@@ -1,5 +1,16 @@
 from PIL import Image
+import cv2
 
-# Открытие изображения
-img = Image.open("sample.png")
-img.show()  # Показать изображение
+# Загрузка изображения
+image = cv2.imread("sample.png")
+if image is None:
+    print("Ошибка: изображение не найдено!")
+else:
+    # Конвертация в RGB (для PIL)
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    pil_image = Image.fromarray(image_rgb)
+
+    # Изменение размера
+    resized_image = pil_image.resize((200, 200))
+    resized_image.save("output.jpg")
+    print("Изображение обработано и сохранено как output.jpg")
